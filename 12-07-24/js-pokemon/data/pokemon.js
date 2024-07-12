03df3163e2d10c1d84045507fab38f8566e9e098
EmanueleGurini <emanuele.gurini89@gmail.com>
2024-07-12 20:53:10 +0200

    Add card

--- 12-07-24/js-pokemon/data/pokemon.js
+++ 12-07-24/js-pokemon/data/pokemon.js
@@ -55,6 +55,4 @@ const pokemon = [
 	}
 ]
 
-export {
-	pokemon
-}
+export { pokemon }
--- 12-07-24/js-pokemon/script.js
+++ 12-07-24/js-pokemon/script.js
@@ -1,2 +1,38 @@
-import { pokemon } from "./data/pokemon";
+import { pokemon } from "./data/pokemon.js";
+
+const cardElGen = (obj) => {
+	// Creiamo la prima card
+	const container = document.createElement('div');
+	const imageEl = document.createElement('img');
+	const titleEl = document.createElement('h2');
+	const idEl = document.createElement('span');
+	const typeEl = document.createElement('p');
+
+
+	container.classList.add('card', obj.type)
+
+	imageEl.src = obj.image;
+	imageEl.className = "card__img"
+
+	titleEl.textContent = obj.name;
+	titleEl.className = "card__title"
+
+
+	idEl.textContent = `# ${obj.id}`;
+	idEl.className = "card__id"
+
+	typeEl.textContent = obj.type;
+	typeEl.className = "card__type"
+
+	container.append(imageEl, titleEl, idEl, typeEl);
+	return container;
+}
+
+const main = document.getElementById('main');
+
+pokemon.forEach((singlePokemon) => {
+	const cardEl = cardElGen(singlePokemon);
+	main.append(cardEl);
+})
+
 
