f77100df903d0579db86dcd44e1258400947e534
EmanueleGurini <emanuele.gurini89@gmail.com>
2024-07-18 20:37:05 +0200

    feat: add filter

--- 18-07-24/react-crud/src/App.jsx
+++ 18-07-24/react-crud/src/App.jsx
@@ -3,6 +3,11 @@ import { Link } from "react-router-dom";
 
 function App() {
   const [pokemonList, setPokemonList] = useState([]);
+  const [filter, setFilter] = useState("");
+
+  const handleChange = (e) => {
+    setFilter(e.target.value);
+  };
 
   useEffect(() => {
     fetch("http://localhost:3000/pokemon")
@@ -25,20 +30,23 @@ function App() {
   return (
     <>
       <h1>Tutti i pokemon che hai catturato!</h1>
+      <input onChange={handleChange} type="text" placeholder="Insert name" />
       <ul>
-        {pokemonList?.map((pokemon) => {
-          return (
-            <li className="flex gap-4 p-1" key={pokemon.id}>
-              <Link to={`/pokemon/${pokemon.id}`}>{pokemon.name}</Link>
-              <button
-                onClick={() => handleDelete(pokemon.id)}
-                className="bg-red-500"
-              >
-                Delete
-              </button>
-            </li>
-          );
-        })}
+        {pokemonList
+          .filter((pokemon) => pokemon.name.includes(filter))
+          .map((pokemon) => {
+            return (
+              <li className="flex gap-4 p-1" key={pokemon.id}>
+                <Link to={`/pokemon/${pokemon.id}`}>{pokemon.name}</Link>
+                <button
+                  onClick={() => handleDelete(pokemon.id)}
+                  className="bg-red-500"
+                >
+                  Delete
+                </button>
+              </li>
+            );
+          })}
       </ul>
     </>
   );
