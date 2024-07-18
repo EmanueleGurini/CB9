8cc0f0d1486aa18e864c74817e8a47dde7d2cd98
EmanueleGurini <emanuele.gurini89@gmail.com>
2024-07-18 20:28:21 +0200

    feat: add brutta delete

--- 18-07-24/react-crud/src/App.jsx
+++ 18-07-24/react-crud/src/App.jsx
@@ -10,14 +10,32 @@ function App() {
       .then((res) => setPokemonList(res));
   }, []);
 
+  const handleDelete = (id) => {
+    fetch(`http://localhost:3000/pokemon/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then(() => {
+        fetch("http://localhost:3000/pokemon")
+          .then((res) => res.json())
+          .then((res) => setPokemonList(res));
+      });
+  };
+
   return (
     <>
       <h1>Tutti i pokemon che hai catturato!</h1>
       <ul>
-        {pokemonList.map((pokemon) => {
+        {pokemonList?.map((pokemon) => {
           return (
-            <li key={pokemon.id}>
+            <li className="flex gap-4 p-1" key={pokemon.id}>
               <Link to={`/pokemon/${pokemon.id}`}>{pokemon.name}</Link>
+              <button
+                onClick={() => handleDelete(pokemon.id)}
+                className="bg-red-500"
+              >
+                Delete
+              </button>
             </li>
           );
         })}
